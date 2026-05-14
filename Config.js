ffffa0900310f38c2816a6931c30629704fd0403
Lughlt <168665654+Klesyolira@users.noreply.github.com>
2026-05-14 01:31:05 -0300

    Add configuration settings to Config.js

--- Config.js
+++ Config.js
@@ -1,1 +1,25 @@
 
+{
+   "Version":2,
+   "Ads": "on",
+   "UpdateUrl":"",
+   "ReleaseNotes":"Default Notes",
+   "Fundo":"https://pbs.twimg.com/media/HIL4ck5aIAATuX3?format=jpg&name=large",
+   "Servers":[
+      {
+         "serverType":0,
+         "Name":"SERVER SSL",
+         "Flag":"us",
+         "Info":"SSH",
+         "tunnelType":2,
+         "ServerHost":"127.0.0.1",
+         "ServerPort":"22",
+         "ProxyHost":"",
+         "ProxyPort":"",
+         "Username":"",
+         "Password":"",
+         "Payload":"",
+         "Sni":"google.com"
+      }
+   ]
+}
